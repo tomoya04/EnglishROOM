88ac407026021b9f7218bb61540675beec4a7ede
tomoya harada <freiheitlibre0420@icloud.com>
2019-01-29 09:52:47 +0000

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザーを登録しました"
+      redirect_to @user
+    else
+      flash.now[:danger]= "ユーザーを登録できませんでした"
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    
+    
+  end
+
+  
+
+  def destroy
+  end
+  
+  def user_params
+   params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+end
